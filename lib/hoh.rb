80b8f2534ff3c5a6f4c40981473dc4fe1b2f960b
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-05 10:48:20 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -17,7 +17,7 @@ def naming_system
         sub_category:{
           label: "Order",
           sub_category: {
-            label: "Genus"
+            label: "Genus",
           }
         }
       }
